f602cd7d5b922af1c6b2559d5e3b9be95328d4f4
TorD <tor.damian@gmail.com>
2016-05-05 16:17:15 +0200

    Inline MIT license

--- TDDP_PreloadManager.js
+++ TDDP_PreloadManager.js
@@ -1,5 +1,28 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Tor Damian Lorentzen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 var Imported = Imported || {};
-Imported.TDDP_PreloadManager = "2.0.0-RC1";
+Imported.TDDP_PreloadManager = true;
 
 var TDDP = TDDP || {}; TDDP.PreloadManager = { config: {
 startupPreload: [ // !! Do not edit this line !!
@@ -45,7 +68,7 @@ indexFilename: ".PM_Index",
 }};
 //=============================================================================
 /*:
- * @plugindesc 2.0.0-RC1 Preload resources on scene/map load as well as game startup for a smoother gameplay experience.          id:TDDP_PreloadManager
+ * @plugindesc 2.0.0-RC2 Preload resources on scene/map load as well as game startup for a smoother gameplay experience.          id:TDDP_PreloadManager
  *
  * @author Tor Damian Design / Galenmereth
  *
